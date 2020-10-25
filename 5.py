80820dc59c9f8eab94ea327dedc1f632bbb84e77
odoggy21 <owenmoogk@gmail.com>
2020-10-24 21:07:57 -0400

    Added questions to code

--- 5.py
+++ 5.py
@@ -1,3 +1,8 @@
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+
+
 for n in range(20,1000000000000):
   if n%20==0 and n%19==0 and n%18==0 and n%17==0 and n%16==0 and n%15==0 and n%14==0 and n%13==0 and n%12==0 and n%11==0:
     print(n)
--- 9.py
+++ 9.py
@@ -1,7 +1,14 @@
+# A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+# a2 + b2 = c2
+# For example, 32 + 42 = 9 + 16 = 25 = 52.
+# There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+# Find the product abc.
+
+
 for c in range (0,1000):
   for b in range (0,c):
     for a in range (0,b):
       if a+b+c==1000:
         if a*a+b*b==c*c:
           print (a,b,c)
-          print(a*b*c)
+          print (a*b*c)
