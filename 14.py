d12f2987a9317b342e88ec55010125ac804cdc91
odoggy21 <owenmoogk@gmail.com>
2020-10-24 20:14:40 -0400

    Update 14.py

--- 14.py
+++ 14.py
@@ -1,19 +1,17 @@
-scounter=0
-finalscounter=0
-scounter = 0
 finalscounter = 0
-
-for n in range(2, 1000000):
-    #n stores og num
-    checkingn = n
-    #checkingn is the number being manipulated
-    while checkingn != 1:
-        if checkingn % 2 == 0:
-            checkingn = checkingn / 2
-            scounter = scounter + 1
-        if checkingn % 2 != 0:
-            checkingn = 3 * checkingn + 1
-            scounter = scounter + 1
-    if scounter > finalscounter:
-        finalscounter = scounter
-        print(n)
+biggest = 0
+print('calculating')
+for n in range(1,1000000):
+  checkingn = n
+  scounter=0
+  while checkingn != 1:
+    if checkingn % 2 == 0:
+      checkingn = checkingn / 2
+      scounter = scounter + 1
+    else:
+      checkingn = (3*checkingn) + 1
+      scounter = scounter + 1
+  if scounter > finalscounter:
+    finalscounter = scounter
+    biggest = n
+print(biggest)
