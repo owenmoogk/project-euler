22a21510c9d6056f0653a67fb0c9ce38fac8d0db
Owen Moogk <58040528+owenmoogk@users.noreply.github.com>
2020-10-26 12:12:01 -0400

    Solved 12

--- 12.py
+++ 12.py
@@ -15,22 +15,31 @@
 
 
 
-finaln=0
-for n in range (1,100000):
-  for i in range (1,n):
-    finaln=finaln+i
-    if i==n-1:
+import math
 
-      x=finaln
-      y=(x**.5)
-      factorcounter=0
 
-      for i in range (1,y+1):
-        if x%i==0:
-          if i==x/i:
-            factorcounter=factorcounter+1
-          else:
-            factorcounter=factorcounter+2
-      if factorcounter>500:
-        print (factorcounter)
-        print (finaln)
+def numOfFactors(num):
+  result = 0
+  for i in range (1,round(math.sqrt(num))+1):
+    if num%i == 0:
+      result += 2
+    if i*i ==num:
+      result = result - 1
+  return (result)
+
+print ('running')
+
+currentNum = 0
+increment = 1
+stop = 0
+
+while stop == 0:
+  currentNum += increment
+  increment +=1
+  x = numOfFactors(currentNum)
+  if x > 100:
+    print (x, currentNum)
+  if x > 500:
+    stop == 1
+    print(currentNum, x)
+    break
